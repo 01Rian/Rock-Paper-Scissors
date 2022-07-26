b99e4d9c9a7a3d9cdaeb3c6293b55abc750c2eaa
Rian <riansantosjr11@gmail.com>
2022-07-26 18:13:56 -0300

    modified function of random choice

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const options = ['Rock', 'Paper', 'Scissors']
+
 function getComputerChoice() {
-    let options = ['Rock', 'Paper', 'Scissors']
     return console.log(options[Math.floor((Math.random() * options.length))])
 }
 
