0f1e91874a692ff4d24cfbf9acd2285fa02f329e
Rian <riansantosjr11@gmail.com>
2022-08-09 19:37:02 -0300

    refactored getComputerChoice func

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const score = document.querySelector('#score');
 const winner = document.querySelector('#winner');
 const loser = document.querySelector('#loser');
 const main = document.querySelector('#main');
+
+function getComputerChoice() {
+    const option = ['rock', 'paper', 'scissor'];
+    let options = option[Math.floor((Math.random() * option.length))];
+    player2 = options;
+    imgPc.innerHTML = "<img src='assets/images/" + player2 + ".png'>";
+    playRound()
+}
